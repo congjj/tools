69f049044c95075c31e2b6f11b0cee04d686d261
CJJ <congjiajia_007@hotmail.com>
2017-12-18 10:44:40 +0800

    xloading

---
+++ library/src/main/java/cjj/tiexi/shenyang/library/xloading/XloadingActivity.java
@@ -0,0 +1,17 @@
+package cjj.tiexi.shenyang.library.xloading;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cjj.tiexi.shenyang.library.R;
+
+public class XloadingActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_xloading);
+    }
+}
